725e51580d5a9cf9fcee640a9cd633676998db03
Sean Murphy <smokebreak@gmail.com>
2017-08-03 10:56:49 -0400

    Add remaining dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    // coffee = require('gulp-coffee'),
+    coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
-    // gulpif = require('gulp-if'),
-    // uglify = require('gulp-uglify'),
-    // minifyHTML = require('gulp-minify-html'),
-    // jsonminify = require('gulp-jsonminify'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 gulp.task('log', function() {
